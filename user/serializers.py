586d169f67c63d3b02e93aac07227f0b293bac78
Dodeja-Shubham <dodejashubham@gmail.com>
2020-09-06 21:40:51 +0530

    Added User Registration API

--- user/serializers.py
+++ user/serializers.py
@@ -7,3 +7,20 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class RegisterSerializer(RegisterSerializer): 
+    class Meta:
+        model = User
+        fields = ['username', 'full_name', 'mobile', 'email', 'password1', 'password2']
+
+    def validate_email(self, email):
+        email = get_adapter().clean_email(email)
+        return email
+    
+    def validate_password1(self, password):
+        return get_adapter().clean_password(password)
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("The two password fields didn't match.")
+        return data
--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from user import views
 urlpatterns = [
     path('', views.UserView.as_view()),
     path('<int:id>', views.User_Each_View.as_view()),
+    path('registration/', include('rest_auth.registration.urls')),
 ]
