26d83b546b5ebacd1146bd80f3e78935f0a9fab3
Dodeja-Shubham <dodejashubham@gmail.com>
2020-09-09 11:52:56 +0530

    Added Migration

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -105,10 +105,9 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 #         'HOST': '127.0.0.1',
 #     }
 # }
-DATABASE_URL = 'postgresql://<postgresql>'
 
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL)
+    'default': dj_database_url.config()
 }
 
 
