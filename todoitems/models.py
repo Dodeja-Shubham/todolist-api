6f41e63b7fed8d68a634c045ca3ebce23bad2065
Dodeja-Shubham <dodejashubham@gmail.com>
2020-09-09 11:51:25 +0530

    Changes category field

--- todoitems/models.py
+++ todoitems/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 # Create your models here.
 class ToDoItem(models.Model):
-    home = 'Home'
-    personal = 'Personal'
-    work = 'Work'
-    fitness = 'Fitness'
-    medication = 'Medication'
-    categories = [
-        (home, 'Home'),
-        (personal, 'Personal'),
-        (work, 'Work'),
-        (fitness, 'Fitness'),
-        (medication, 'Medication'),
-    ]
-    category = models.CharField(max_length=10, choices=categories, default=1)
+    # home = 'Home'
+    # personal = 'Personal'
+    # work = 'Work'
+    # fitness = 'Fitness'
+    # medication = 'Medication'
+    # categories = [
+    #     (home, 'Home'),
+    #     (personal, 'Personal'),
+    #     (work, 'Work'),
+    #     (fitness, 'Fitness'),
+    #     (medication, 'Medication'),
+    # ]
+    category = models.CharField(max_length=100, default=1)
     title = models.CharField(max_length=150)
     due_date = models.DateField(null=True, blank=True)
     desc = models.TextField(null= True)
